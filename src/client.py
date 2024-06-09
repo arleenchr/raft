f64888054ead8ada111474cf656e97505cbdc82b
kevinjohn01 <16521168@std.stei.itb.ac.id>
2024-06-09 23:37:04 +0700

    fix: add all command in client

--- src/client.py
+++ src/client.py
@@ -1,17 +1,31 @@
 import xmlrpc.client
 import json
 
-server = xmlrpc.client.ServerProxy("http://localhost:8080")
+server = xmlrpc.client.ServerProxy("http://localhost:8090")
 
-request = {'service': 'get', 'params': '1'}
 # request = {'service': 'ping', 'params': ''}
-# request = {'service': 'set', 'params': {'key': 'halo', 'value':'SI'}}
-# request = {'service': 'set', 'params': {'key': '1', 'value':'A'}}
-# request = {'service': 'set', 'params': {'key': '2', 'value':'SI'}}
-# request = {'service': 'delete', 'params': {'key':'halo'}}
+request = {'service': 'set', 'params': {'key': '1', 'value':'A'}}
 # request = {'service': 'append', 'params': {'key': '1', 'value':'BC'}}
+# request = {'service': 'set', 'params': {'key': '2', 'value':'SI'}}
 # request = {'service': 'append', 'params': {'key': '2', 'value':'S'}}
+# request = {'service': 'get', 'params': '2'}
+
+# request = {'service': 'strln', 'params': {'key': '1'}}
 # request = {'service': 'strln', 'params': {'key': '2'}}
+# request = {'service': 'delete', 'params': {'key':'1'}}
+# request = {'service': 'append', 'params': {'key': '2', 'value':'TE'}}
+# request = {'service': 'append', 'params': {'key': '2', 'value':'R'}}
+
+# request = {'service': 'set', 'params': {'key': '3', 'value':''}}
+# request = {'service': 'append', 'params': {'key': '3', 'value':'UwU'}}
+# request = {'service': 'append', 'params': {'key': '4', 'value':'Onii-Chan'}}
+# request = {'service': 'append', 'params': {'key': '4', 'value':'Daisuki'}}
+
+# request = {'service': 'set', 'params': {'key': 'halo', 'value':'SI'}}
+# request = {'service': 'get', 'params': '2'}
+# request = {'service': 'get', 'params': '3'}
+# request = {'service': 'get', 'params': '4'}
+
 # request = {'service': 'request_log'}
 # request = {
 #             "term":             0,
